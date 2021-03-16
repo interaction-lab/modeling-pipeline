bd3ec20cf33b640780b05dcd2df8e764c5e5c210
chrismbirmingham <cmbirmingham19@gmail.com>
2021-03-16 12:15:06 -0700

    Update import tests

--- examples/test_imports.py
+++ examples/test_imports.py
@@ -1,7 +1,16 @@
-# Run scripts from the root, e.g.
-# python examples/test_imports.py
-import pipeline.common.file_utils as fu
-import pipeline.modeling.data_utils as dat
-import pipeline.annotation.annotate_utterances as au
+# Run scripts from the root, as module e.g.
+    # python -m examples.test_imports
 
-print("success")
+import sys
+
+for p in sys.path:
+    print(p)
+
+try:
+    import pipeline.common.file_utils as fu
+    import pipeline.modeling.data_utils as dat
+    import pipeline.annotation.annotate_utterances as au
+
+    print("success")
+except ModuleNotFoundError as e:
+    raise(e)
